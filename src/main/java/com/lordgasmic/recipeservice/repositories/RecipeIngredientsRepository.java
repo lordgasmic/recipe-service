6cd9ed5cdd983ea4b26d837ab7d42e0141119180
Nick Wellman <wellman.nicholas@gmail.com>
2025-12-08 11:26:35 -0500

    fix repo definition. clean up deps

--- src/main/java/com/lordgasmic/recipeservice/repositories/RecipeIngredientsRepository.java
+++ src/main/java/com/lordgasmic/recipeservice/repositories/RecipeIngredientsRepository.java
@@ -1,8 +1,9 @@
 package com.lordgasmic.recipeservice.repositories;
 
+import com.lordgasmic.recipeservice.entities.RecipeIngredientEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RecipeIngredientsRepository extends CrudRepository<RecipeIngredientsRepository, Long> {
+public interface RecipeIngredientsRepository extends CrudRepository<RecipeIngredientEntity, Long> {
 }
